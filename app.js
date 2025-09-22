d5ca9eb908776842fdfb9a4d7d3d4a6736038e52
Kepham123 <kennypham6@icloud.com>
2025-09-22 23:16:27 +0000

    Created HTTP server and parse URL for routing. Added switch statement to handle /schedule and /cancel paths. Implemented availability check for the schedule function. Added a reusable error function for handling responses.

--- app.js
+++ app.js
@@ -1,4 +1,52 @@
 
+const http = require('http');
+const url = require('url');
+//create a server object:
+let myserver = http.createServer(function (req, res) {
+	let urlObj = url.parse(req.url,true);
+	console.log(req.url);
+	switch (urlObj.pathname){
+		case "/schedule":
+			schedule(urlObj.query,res);
+			break
+		case "/cancel":
+			cancel();
+			break;
+		default:
+			error(404,"pathname not found", res);
+	}
+ res.writeHead(200, {'Content-Type': 'text/plain'});
+ res.write("recieved"); //write (send) a response to the client
+ res.end(); //end the response
+});
+myserver.listen(80); //the server object listens on port 80
+
+function schedule(queryObj,res) {
+	if (availableTime[queryObj.day].some(element => element == queryObj.time))
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+ 		res.write("Scheduled"); //write (send) a response to the client
+ 		res.end(); //end the response
+	}
+	else;
+		error(400, "unable to schedule",res);
+
+}
+function cancel() {
+
+
+
+}
+function error(status, message,res) {
+	res.writeHead(status, {'Content-Type': 'text/plain'});
+	res.write(message); //write (send) a response to the client
+	res.end(); //end the response
+}
+
+  
+
+
+}
+
 const availableTimes = {
     Monday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
     Tuesday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
